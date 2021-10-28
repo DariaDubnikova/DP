1d904f06592f50c18844354837772cb3a267d6bf
DariaDubnikova <d.dasha20010513@gmail.com>
2021-10-28 10:07:36 +0300

    added pa3

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -2,3 +2,7 @@ start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5001"
 start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5002"
 
 start /d ..\nginx\ nginx.exe
+start /d ..\nats\ nats-server.exe
+
+start "Rank calculator 1" /d ..\RankCalculator\ dotnet run 
+start "Rank calculator 2" /d ..\RankCalculator\ dotnet run 
